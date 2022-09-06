645c3cc713319fcee50a15a7af6a15b820a50686
zhao_jianping <zhao_jianping@cdv.com>
2022-09-06 14:54:36 +0800

    feat: 文章模型设置

---
+++ mongoose/modules/articles.js
@@ -0,0 +1,104 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: mongoose.Types.ObjectId
+  },
+  // 标题
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  // 简介
+  desc: {
+    type: String,
+    default: null
+  },
+  // 标签
+  tags: {
+    type: [String],
+    default: []
+  },
+  // 作者
+  auth: {
+    type: String,
+    default: ""
+  },
+  // 创建者
+  createBy: {
+    type: String,
+    required: true
+  },
+  // 修改者
+  modifiedBy: {
+    type: String,
+    required: true
+  },
+  // 属性
+  extraData: {
+    // 内容
+    content: {
+      type: String,
+      default: ""
+    },
+    // 原创
+    original: {
+      type: String,
+      default: "original"
+    },
+    // 文章类别
+    category: {
+      type: [String],
+      default: []
+    },
+    // 封面模式
+    coverImgPattern: {
+      type: String,
+      default: "default1"
+    },
+    // 封面图1:1
+    coverImg1x1: {
+      type: [String],
+      default: []
+    },
+    // 封面图4:3
+    coverImg4x3: {
+      type: [String],
+      default: []
+    },
+    // 封面图16:9
+    coverImg16x9: {
+      type: [String],
+      default: []
+    },
+    // 可见范围
+    readScope: {
+      type: String,
+      default: "公开"
+    }
+  },
+  // 创建时间
+  createdTime: {
+    type: Date,
+    default: Date.now()
+  },
+  // 修改时间
+  modifiedTime: {
+    type: Date,
+    default: Date.now()
+  },
+  // 发布时间
+  publishTime: {
+    type: Date,
+    default: null
+  },
+  // 首次发布时间
+  firstPublishTime: {
+    type: Date,
+    default: null
+  }
+})
+
+module.exports = mongoose.model("Article", articleSchema);
