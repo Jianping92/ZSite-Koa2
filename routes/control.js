8e41528400a1ec1850a1891a07be02b64a20cb45
zhao_jianping <zhao_jianping@cdv.com>
2022-09-13 15:12:40 +0800

    feat: 文章查询接口设计初版

--- routes/control.js
+++ routes/control.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')();
-const Article = require("../mongoose/modules/articles")
+const Article = require("../mongoose/modules/articles");
+const Utils = require("../utils/index");
 router.prefix('/control');
 
 router.post("/createArticle", async (ctx) => {
@@ -37,6 +38,37 @@ router.post("/createArticle", async (ctx) => {
     ctx.body = {
       code: -1,
       message: "文章保存失败，请稍后尝试！"
+    }
+  }
+})
+
+router.post("/queryArticle", async (ctx) => {
+  const body = ctx.request.body;
+  const { orders, limit, start, group = undefined } = body;
+  const dbCommand = Utils.getDbQueryCommand2(group);
+  if (!dbCommand) {
+    ctx.body = {
+      code: -1,
+      message: "请求参数错误"
+    }
+    return;
+  }
+  // 合并查询条件
+  const sort = orders.length ? Object.assign(...orders) : {};
+  // skip: 跳过的文档数量，本次设计的分页传值中的start为从第几位开始查询，因此也就相当于跳过多少位
+  // limit: 指定放回结果的最大数量，也就是分页中每页显示多少条
+  // sort: 按照排序规则根据所给的字段进行排序，值可以是 asc, desc, ascending, descending, 1, 和 -1
+  // sort: 不区分大小写，如果多个条件，需要将查询条件合并到同一个object中，如果没有查询条件则传递{}
+  // find: 筛选条件
+  const list = await Article.find(dbCommand).skip(start).limit(limit).sort(sort);
+  // countDocuments(): 获取总数，参数为object，用于筛选
+  const listTotal = await Article.countDocuments(dbCommand);
+  if (list) {
+    ctx.body = {
+      code: 200,
+      message: "success",
+      body: [...list],
+      total: listTotal
     }
   }
 })
---
+++ utils/index.js
@@ -0,0 +1,81 @@
+/**
+ * 对key进行转换
+ * */
+const getDbKey = {
+  "allOf": "$all",
+  "anyOf": "$or",
+  "LK": "$in",
+  "IN": "$in",
+  "NIN": "$nin",
+  "EQ": "$eq",
+  "NE": "$ne"
+}
+
+/**
+ * 处理请求查询语句
+ * 2022-09-13 废弃，不能模仿这种写法
+ * */
+const getDbQueryCommand = group => {
+  if ("allOf" in group && "anyOf" in group) {
+    return null;
+  }
+  // 递归处理key中的搜索关键词
+  const recursiveReplacement = data => {
+    const objArray = Object.entries(data)[0];
+    if (!getDbKey[objArray[0]]) {
+      const newData = {};
+      newData[objArray[0]] = recursiveReplacement(objArray[1]);
+      return newData;
+    } else if (getDbKey[objArray[0]] === "$all" || getDbKey[objArray[0]] === "$or") {
+      const newData = {};
+      const newDataArray = [];
+      objArray[1].forEach(item => {
+        newDataArray.push(recursiveReplacement(item));
+      })
+      newData[getDbKey[objArray[0]]] = newDataArray;
+      return newData;
+    } else {
+      const newData = {};
+      newData[getDbKey[objArray[0]]] = objArray[1];
+      return newData;
+    }
+  }
+  let command = {};
+  if ("allOf" in group) {
+    command[getDbKey["allOf"]] = [];
+    group["allOf"].forEach(item => {
+      command[getDbKey["allOf"]].push(recursiveReplacement(item));
+    })
+  } else if ("anyOf" in group) {
+    command[getDbKey["anyOf"]] = [];
+    group["anyOf"].forEach(item => {
+      command[getDbKey["anyOf"]].push(recursiveReplacement(item));
+    })
+  }
+  return command;
+}
+
+/**
+ * 处理请求查询语句2
+ * */
+const getDbQueryCommand2 = group => {
+  if (!group) {
+    return {};
+  }
+  let objArray = Object.entries(group);
+  objArray = objArray.map(item => {
+    const value = Object.entries(item[1])[0];
+    if (getDbKey[value[0]]) {
+      item[1] = {}
+      item[1][getDbKey[value[0]]] = value[1];
+    }
+    let newItem = {};
+    newItem[item[0]] = item[1];
+    return newItem;
+  })
+  return objArray.length ? Object.assign({}, ...objArray) : {};
+}
+
+module.exports = {
+  getDbQueryCommand2
+}
